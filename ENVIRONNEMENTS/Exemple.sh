ac98f7221b71f6c5a979a282919dc4c03504e95d
Marc <teste74@hotmail.fr>
2021-12-18 11:36:57 +0100

    Update Exemple.sh

--- ENVIRONNEMENTS/Exemple.sh
+++ ENVIRONNEMENTS/Exemple.sh
@@ -8,8 +8,8 @@ echo '' > /etc/environment ;
 #############################
 # Information de la Machine #
 #############################
-export HOSTNAME=$(hostname)
-export DOMAINE=lan
+export HOSTNAME=
+export DOMAINE=
 export TIMEZONE=
 export REGION=Europe
 export VILLE=Paris
@@ -31,7 +31,6 @@ export NET_DNS1=$(ip add | cut -c 10-18 | grep "192").40
 # Clé SSH #
 ###########
 export SSH_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAs+2hvOiFJvMh7Tdmle+bAYLwEyErLFYE74ljUny/2D2zl6XTZmBw7JEdD3H9mXqeJTk/BGEa2Sn4G0OkwrYH6xnXlwsLRB1Wosa2Q2vX1vG1MxU6e/t7ngsQ74wHGj6Nq5hNZP7EfTfTA6Jo26k9BU5+qj1C4Oq9uvurZuwVl9MYVX59CQb5jKIqgB/obllICqNaGk+msxDSEKh2gFzcEHLa0Kf8R6t+z1m79M+X5I0SLosQluc4edfj77Y3OqPNBooDt5i3MqAJWvzagGStQN2RSs3LUPaMSMleew2qKZEHRzl9u75MaAtKIpH7qe4xHPprDXAF1bKIa2PTNRv9HQ== teste74@hotmail.fr"
-
 
 ##################
 # User & Groupes #
