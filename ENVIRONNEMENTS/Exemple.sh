965a848a68dcfe370d049b8b40cf10c15661d904
Marc <teste74@hotmail.fr>
2021-12-18 19:07:43 +0100

    Update Exemple.sh

--- ENVIRONNEMENTS/Exemple.sh
+++ ENVIRONNEMENTS/Exemple.sh
@@ -3,72 +3,65 @@
 ######################################################
 
 # Vider les variables Environnements
-echo '' > /etc/environment ;
+echo '' > ./environment ;
 
 
-##############################
+
+echo "##############################
 # Information sur le Système #
 ##############################
-export DISTRIB=$(lsb_release -a | grep "^Codename" | cut -c 11-20)
-export RELEASE=$(lsb_release -a | grep "^Distributor" | cut -d ":" -f 2 | cut -c 2-10)
+DISTRIB='$(lsb_release -a | grep '^Codename' | cut -c 11-20)'
+RELEASE='$(lsb_release -a | grep '^Distributor' | cut -d ':' -f 2 | cut -c 2-10)'
 
 
 #############################
 # Information de la Machine #
 #############################
-export HOSTNAME=Debian
-export DOMAINE=Lan
-export REGION=Europe
-export VILLE=Paris
-export LANGUE=fr_FR.UTF-8
+HOSTNAME=''
+DOMAINE=''
+REGION='Europe'
+VILLE='Paris'
+LANGUE='fr_FR.UTF-8'
 
 #########################
 # Information le Réseau #
 #########################
 
-export NET_LOOPBACK=$(ip link | grep "^1:" | cut -c 4-20 |cut -d ":" -f 1)
-export NET_INTERFACE1=$(ip link | grep "^2:" | cut -c 4-20 |cut -d ":" -f 1)
-export NET_INTERFACE2=$(ip link | grep "^3:" | cut -c 4-20 |cut -d ":" -f 1)
-export NET_ADDRESS=$(ip add | cut -c 10-30 | grep "192" | cut -d " " -f 1)
-export NET_GATEWAY=$(ip add | cut -c 10-18 | grep "192").1
-export NET_DNS0=$(ip add | cut -c 10-18 | grep "192").1
-export NET_DNS1=$(ip add | cut -c 10-18 | grep "192").40
+# Récupérer Nom Interface:
+NET_LOOPBACK='$(ip link | grep '^1:' | cut -c 4-20 |cut -d ':' -f 1)'
+NET_INTERFACE1='$(ip link | grep '^2:' | cut -c 4-20 |cut -d ':' -f 1)'
+NET_INTERFACE2='$(ip link | grep '^3:' | cut -c 4-20 |cut -d ':' -f 1)'
+NET_ADDRESS='192.168.1.'
+NET_GATEWAY='192.168.1.1'
+NET_DNS0='192.168.1.1'
+NET_DNS1='192.168.1.40'
+
 
 ###########
 # Clé SSH #
 ###########
-export SSH_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAs+2hvOiFJvMh7Tdmle+bAYLwEyErLFYE74ljUny/2D2zl6XTZmBw7JEdD3H9mXqeJTk/BGEa2Sn4G0OkwrYH6xnXlwsLRB1Wosa2Q2vX1vG1MxU6e/t7ngsQ74wHGj6Nq5hNZP7EfTfTA6Jo26k9BU5+qj1C4Oq9uvurZuwVl9MYVX59CQb5jKIqgB/obllICqNaGk+msxDSEKh2gFzcEHLa0Kf8R6t+z1m79M+X5I0SLosQluc4edfj77Y3OqPNBooDt5i3MqAJWvzagGStQN2RSs3LUPaMSMleew2qKZEHRzl9u75MaAtKIpH7qe4xHPprDXAF1bKIa2PTNRv9HQ== teste74@hotmail.fr"
+SSH_KEY=''
 
 ##################
 # User & Groupes #
 ##################
-export USER=
-export GROUP=
+USER='marc'
+GROUP='administrateur'
 
 ################
 # Mot de passe #
 ################
-export PASS=
+PASS='admin'
 
 ##################
 # ID USER, Group #
 ##################
-export UID=
-export GUID=
-
+UID='1001'
+GUID='1001'
 
 ###############
 # APPZ: SAMBA #
 ###############
-export APPZ_SAMBA_USER=
-export APPZ_SAMBA_PASS=
-
-
-
-echo "####### Interface $NET_INTERFACE1 #######
- - CIDR     : $NET_ADDRESS 
- - GateWay  : $NET_GATEWAY
- - DNS0     : $NET_DNS0
- - DNS1     : $NET_DNS1
- - Domaine  : $DOMAINE
-###############################"
+APPZ_SAMBA_USER=
+APPZ_SAMBA_PASS=
+" > ./environment; clear; cat ./environment
