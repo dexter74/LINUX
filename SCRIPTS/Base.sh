e9f7fee4a0123a63c4440b106fa7b5d8ee9202c1
Marc <teste74@hotmail.fr>
2021-12-18 11:37:47 +0100

    Create Base.sh

---
+++ SCRIPTS/Base.sh
@@ -0,0 +1,19 @@
+###################################
+# Script de Configuration du Host #
+###################################
+
+hostnamectl set-hostname $HOSTNAME ;
+
+
+#######################################
+# Script de Configuration du Hostname #
+#######################################
+echo "#IPV4
+127.0.0.1       localhost
+127.0.1.1       $HOSTNAME        $HOSTNAME
+#127.0.1.1      $HOSTNAME.$DOMAINE    $HOSTNAME
+
+#IPV6 (OFF)
+::1     localhost ip6-localhost ip6-loopback
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters" > /etc/hosts ;
