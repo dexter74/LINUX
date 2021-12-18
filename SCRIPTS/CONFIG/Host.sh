11f2ccf8e949d8037dc1696cc1740f176a55d531
Marc <teste74@hotmail.fr>
2021-12-18 23:00:33 +0100

    Update Host.sh

--- SCRIPTS/CONFIG/Host.sh
+++ SCRIPTS/CONFIG/Host.sh
@@ -2,7 +2,7 @@
 # Script de Configuration du Host #
 ###################################
 
-hostnamectl set-hostname '$HOSTNAME' ;
+hostnamectl set-hostname $HOSTNAME ;
 
 
 #######################################
@@ -10,10 +10,12 @@ hostnamectl set-hostname '$HOSTNAME' ;
 #######################################
 echo "#IPV4
 127.0.0.1       localhost
-127.0.1.1       '$HOSTNAME'        '$HOSTNAME'
-#127.0.1.1      '$HOSTNAME'.'$DOMAINE'    '$HOSTNAME'
+127.0.1.1       $HOSTNAME        $HOSTNAME
+#127.0.1.1      $HOSTNAME.$DOMAINE    $HOSTNAME
 
 #IPV6 (OFF)
 ::1     localhost ip6-localhost ip6-loopback
 ff02::1 ip6-allnodes
-ff02::2 ip6-allrouters" > /etc/hosts ;
+ff02::2 ip6-allrouters" > /etc/hosts;
+
+clear; cat /etc/hosts; cat /etc/hostname;
