80c66454e177c4fee630074ef997875fd1680765
Marc <teste74@hotmail.fr>
2021-12-18 11:03:15 +0100

    Create Exemple.sh

---
+++ ENVIRONNEMENTS/Exemple.sh
@@ -0,0 +1,57 @@
+######################################################
+# Script de déclaration de variables d'environnement #
+######################################################
+
+# Vider les variables Environnements
+echo '' > /etc/environment ;
+
+#############################
+# Information de la Machine #
+#############################
+export HOSTNAME=
+export DOMAINE=
+export TIMEZONE=
+export REGION=
+export LANGUE=
+export VILLE=
+
+#########################
+# Information le Réseau #
+#########################
+export NET_INTERFACE=
+export NET_BROADCAST=
+export NET_ADDRESS=
+export NET_GATEWAY=
+export NET_MASK=
+export NET_DNS0=
+export NET_DNS1=
+
+###########
+# Clé SSH #
+###########
+export SSH_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAs+2hvOiFJvMh7Tdmle+bAYLwEyErLFYE74ljUny/2D2zl6XTZmBw7JEdD3H9mXqeJTk/BGEa2Sn4G0OkwrYH6xnXlwsLRB1Wosa2Q2vX1vG1MxU6e/t7ngsQ74wHGj6Nq5hNZP7EfTfTA6Jo26k9BU5+qj1C4Oq9uvurZuwVl9MYVX59CQb5jKIqgB/obllICqNaGk+msxDSEKh2gFzcEHLa0Kf8R6t+z1m79M+X5I0SLosQluc4edfj77Y3OqPNBooDt5i3MqAJWvzagGStQN2RSs3LUPaMSMleew2qKZEHRzl9u75MaAtKIpH7qe4xHPprDXAF1bKIa2PTNRv9HQ== teste74@hotmail.fr"
+
+
+##################
+# User & Groupes #
+##################
+export USER=
+export GROUP=
+
+################
+# Mot de passe #
+################
+export PASS=
+
+##################
+# ID USER, Group #
+##################
+export UID=
+export GUID=
+
+
+###############
+# APPZ: SAMBA #
+###############
+export APPZ_SAMBA_USER=
+export APPZ_SAMBA_PASS=
