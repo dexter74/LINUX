2fb941847b72d1780092874331b2f274d1fea74c
Marc <teste74@hotmail.fr>
2021-12-19 10:50:58 +0100

    Update 3.users_groups.sh

--- Installation/CONFIG/3.users_groups.sh
+++ Installation/CONFIG/3.users_groups.sh
@@ -5,7 +5,12 @@ if [ -z $1 ]
 # ------------------------------------------------------------
  then
   clear;
-  echo "Argument Manquant"
+  echo "Argument Manquant:
+  - DELETE    <USER> <GROUP>
+  - ADD_GROUP <USER> <GID>
+  - ADD_USER  <USER> <UID> <GID>
+  - PASS      <USER> <NEw_PASS>
+  "
 
 # ------------------------------------------------------------
  elif [ $1 = "DELETE" ]
@@ -15,12 +20,14 @@ if [ -z $1 ]
    delgroup $3;
    rm -r    /home/$2;
 
+
 # ------------------------------------------------------------
 # $2: Group | $3: Group_ID
  elif [ $1 = "ADD_GROUP" ]
   then
    clear;
    addgroup $2 --gid $3;
+
 
 # ------------------------------------------------------------
 # $2 = USER | $3 USER_ID | $4 GROUP_ID
@@ -36,19 +43,22 @@ if [ -z $1 ]
    --shell /bin/bash \
    --create-home /home/$2;
 
+
 # ------------------------------------------------------------
 # $2: USER | $3: PASS
  elif [ $1 = "PASS" ]
   then
    clear
    echo "$2:$3" | chpasswd $2;
-   
+
+
 # ------------------------------------------------------------
 # $2: USER
  elif [ $1 = "ROOT" ]
   then
    clear
     echo "$USER ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$2;
+
 # ------------------------------------------------------------
  else
   echo "Argument non reconnu"
