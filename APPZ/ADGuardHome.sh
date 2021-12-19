cc2fe906fa6b5bee71937d597833003b813c5eb0
Marc <teste74@hotmail.fr>
2021-12-19 01:43:08 +0100

    Update ADGuardHome.sh

--- APPZ/ADGuardHome.sh
+++ APPZ/ADGuardHome.sh
@@ -2,21 +2,20 @@
 # ./ADGuardHome.sh <CLEAN> <DOWNLOAD> <ACTION> #
 ################################################
 clear;
-
-# ------------------------------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------------------------$
 if [ -z $1 ]
  then
-  echo 'Merci de préciser un paramètre (PURGE, DL, ENABLE, DISABLE, START, STOP, STATUS)'
+  echo 'Merci de preciser un parametre (PURGE, DL, ENABLE, DISABLE, START, STOP, STATUS)'
 # --------------------------------------------
  elif [ "$1" = "PURGE" ]
   then
-   /opt/AdGuardHome/AdGuardHome -s uninstall;
-   rm -r /opt/AdGuardHome;
-# --------------------------------------------
+   /opt/AdGuardHome/AdGuardHome -s uninstall
+   rm -r /opt/AdGuardHome
+# -------------------------------------------
  elif [ "$1" = "DL" ]
   then
-   wget https://static.adguard.com/adguardhome/release/AdGuardHome_linux_amd64.tar.gz -O /tmp/AdGuardHome_linux_amd64.tar.gz ;
-   tar -xf /tmp/AdGuardHome_linux_amd64.tar.gz -C /opt;
+   wget https://static.adguard.com/adguardhome/release/AdGuardHome_linux_amd64.tar.gz -O /tmp/AdGuardHome_linux_amd64.tar.gz
+   tar -xf /tmp/AdGuardHome_linux_amd64.tar.gz -C /opt
 # --------------------------------------------
  elif [ "$1" = "ENABLE" ]
   then
@@ -33,12 +32,12 @@ if [ -z $1 ]
  elif [ "$1" = "STOP" ]
   then
    /opt/AdGuardHome/AdGuardHome -s stop
- else
 # --------------------------------------------
  elif [ "$1" = "STATUS" ]
   then
    /opt/AdGuardHome/AdGuardHome -s status
+# --------------------------------------------
  else
-  echo 'Le paramètre $1 est inconnu'
+  echo "Le parametre '$1' est inconnu"
 fi
-# ------------------------------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------------------------$
