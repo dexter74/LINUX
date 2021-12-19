89c8b7268433d979c55a70603d60263ede9f9b42
Marc <teste74@hotmail.fr>
2021-12-19 08:32:59 +0100

    Update environnement.sh

--- Installation/CONFIG/environnement.sh
+++ Installation/CONFIG/environnement.sh
@@ -13,39 +13,38 @@ echo VALEUR=\'$1\' > RESULT
 
 
 clear;
-#Fichier Profil:
-SYSOUT=/etc/profile.d/ENVIRONNEMENT.sh
+#Fichier Profil: /etc/profile.d/ENVIRONNEMENT.sh
+SYSOUT=out
+echo "" > $SYSOUT
+echo ############################## >> out
+echo # Information sur le Système # >> out
+echo ############################## >> out
+echo export DISTRIB=\'$1\' >> out
+echo export RELEASE=\'$2\' >> out
+echo export HOSTNAME=\'$3\' >> out
+echo export DOMAINE=\'$4\' >> out
+echo export REGION=\'$5\' >> out
+echo export VILLE=\'$6\' >> out
+echo export LANGUE=\'$7\' >> out
+echo export SSH_KEY=\'MAKEY\' >> out
+echo ############################ >> out
+echo # Information sur le Rseau # >> out
+echo ############################ >> out
+echo export NET_LOOPBACK=\'$(ip link   | grep '^1:' | cut -c 4-20 |cut -d ':' -f 1)\' >> out
+echo export NET_INTERFACE1=\'$(ip link | grep '^2:' | cut -c 4-20 |cut -d ':' -f 1)\' >> out
+echo export NET_INTERFACE2=\'$(ip link | grep '^3:' | cut -c 4-20 |cut -d ':' -f 1)\' >> out
+echo export NET_ADDRESS=\'$8\' >> out
+echo export NET_GATEWAY=\'$9\' >> out
+echo export NET_DNS0=\'$10\' >> out
+echo export NET_DNS1=\'$11\' >> out
+echo ############################ >> out
+echo # Information sur le Rseau # >> out
+echo ############################ >> out
+echo export USER=\'$12\' >> out
+echo export GROUP=\'$13\' >> out
+echo export PASS=\'$14\' >> out
+echo export USER_ID=\'$15\' >> out
+echo export GROUP_UID=\'$16\' >> out
+echo export APPZ_SAMBA_USER=\'$17\' >> out
+echo export APPZ_SAMBA_PASS=\'$18\' >> out
 
-echo '##############################  '
-echo '# Information sur le Système #  '
-echo '##############################  '
-echo 'export DISTRIB='$1'             '
-echo 'export RELEASE='$2'             '
-echo 'export HOSTNAME='$3'            '
-echo 'export DOMAINE='$4'             '
-echo 'export REGION='$5'              '
-echo 'export VILLE='$6'               '
-echo 'export LANGUE='$7'              '
-echo 'export SSH_KEY='XXX'            '
-echo '############################    '
-echo '# Information sur le Rseau #    '
-echo '############################    '
-echo 'export NET_ADDRESS='$8'         '
-echo 'export NET_GATEWAY='$9'         '
-echo 'export NET_DNS0='$10'           '
-echo 'export NET_DNS1='$11'           '
-echo '############################    '
-echo '# Information sur le Rseau #    '
-echo '############################    '
-echo 'export USER='$12'               '
-echo 'export GROUP='$13'              '
-echo 'export PASS='$14'               '
-echo 'export USER_ID='$15'            '
-echo 'export GROUP_UID='$16'          '
-echo 'export APPZ_SAMBA_USER='$17'    '
-echo 'export APPZ_SAMBA_PASS='$18'    '
-
-
-#echo 'export NET_LOOPBACK=$(ip link   | grep "^1:" | cut -c 4-20 |cut -d ":" -f 1)
-#echo 'export NET_INTERFACE1=$(ip link | grep "^2:" | cut -c 4-20 |cut -d ":" -f 1)
-#echo 'export NET_INTERFACE2=$(ip link | grep "^3:" | cut -c 4-20 |cut -d ":" -f 1)
