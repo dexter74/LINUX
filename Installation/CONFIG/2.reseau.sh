8107a422b156ad0507d366813542b6b82c33e6ed
Marc <teste74@hotmail.fr>
2021-12-19 10:05:37 +0100

    Update 2.reseau.sh

--- Installation/CONFIG/2.reseau.sh
+++ Installation/CONFIG/2.reseau.sh
@@ -2,48 +2,64 @@
 # Script de Configuration des Interfaces Réseaux #
 ##################################################
 
-
 ################################
 # Desactiver le protocole IPV6 #
 ################################
-echo "net.ipv6.conf.all.disable_ipv6 = 1" > /etc/sysctl.d/70-disable-ipv6.conf ;
 
-#####################################################
-# Configuration des Interfaces Réseaux en mode DHCP #
-#####################################################
-if [ "$1" = "DHCP" ]
+if [ -z $1 ]
+# ----------------------------
  then
-  clear;
-  echo "# Interface de bouclage
+ echo "Argument valide: DHCP, STATIC"
+# ----------------------------
+ elif [ $1 = "DHCP" ]
+  then
+clear;
+echo "#########################
+# Interface de bouclage #
+#########################
 auto $NET_LOOPBACK
 iface $NET_LOOPBACK inet loopback
 #
-# Interface Physique
+######################
+# Interface Physique #
+######################
 auto $NET_INTERFACE1
 allow-hotplug $NET_INTERFACE1
 iface $NET_INTERFACE1 inet dhcp" > /etc/network/interfaces;
 cat  /etc/network/interfaces ;
- else
-  echo "Merci d'indiquer un parametre: DHCP, STATIC"
-fi
-
-
-
-if [ -z $1 ]
- then
-  echo "Aucun Parametre"
 # ----------------------------
- elif [ $1 = "DHCP" ]
-  then
-  echo "Réseau en DHCP"
-# ----------------------------
-
  elif [ $1 = "STATIC" ]
   then
-  echo "Réseau en Static"
+clear;
+echo "#########################
+# Interface de bouclage #
+#########################
+auto $NET_LOOPBACK
+iface $NET_LOOPBACK inet loopback
+
+######################
+# Interface Physique #
+######################
+auto $NET_INTERFACE1
+allow-hotplug $NET_INTERFACE1
+
+iface $NET_INTERFACE1 inet static
+  address $NET_ADDRESS
+  netmask 255.255.255.0
+  broadcast 192.168.1.255
+  gateway $NET_GATEWAY
+  dns-nameservers $DOMAINE" > /etc/network/interfaces;
+cat  /etc/network/interfaces ;
+
+
+# ----------------------------
+ elif [ $1 = "IV6_OFF" ]
+  then
+   echo "net.ipv6.conf.all.disable_ipv6 = 1" > /etc/sysctl.d/70-disable-ipv6.conf ;
 # ----------------------------
  else
-  echo "Mauvais Paramètre"
+ echo "Script en Erreur"
 # ----------------------------
 fi
+
 
