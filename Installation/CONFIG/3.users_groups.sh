9cfd2f7f74037666f075843b64ddb92b8189eaea
Marc <teste74@hotmail.fr>
2021-12-19 12:37:35 +0100

    Update 3.users_groups.sh

--- Installation/CONFIG/3.users_groups.sh
+++ Installation/CONFIG/3.users_groups.sh
@@ -5,76 +5,102 @@ if [ -z $1 ]
 # ------------------------------------------------------------
  then
   clear;
-  echo "Argument Manquant:
-  - DELETE    <USER> <GROUP>
-  - ADD_GROUP <GID>  <GROUP>
-  - ADD_USER  <USER> <UID> <GID>
-  - PASS      <USER> <NEw_PASS>
-  - ROOT      <USER> (Expérimental)
-  - CHECK     <USER> <GROUP>
-  "
+	echo "Argument Disponible:
+	- DEL_USER       <USER>
+	- DEL_USER_HOME  <USER>
+	- DEL_GROUP      <GROUP>
+	- ADD_GROUP      <GROUP> <GID>
+	- ADD_USER       <USER>  <UID> <GROUP Primaire> <Group secondaire>
+	- DEL_USER_GROUP <USER>  <GROUP>
+	- ADD_USER_GROUP <USER>  <GROUP>
+	- PASSWORD       <USER>  <PASS>
+	- CHECK          <USER>
+	- ADD_SUDOER	 <USER>
+	- DEL_SUDOER	 <USER>
+	"
 
 
 # ------------------------------------------------------------
- elif [ $1 = "DELETE" ]
+ elif [ $1 = "DEL_USER" ]
   then
    clear;
-   deluser  $2;
-   delgroup $3;
-   rm -r    /home/$2;
+	deluser $2;
 
 
 # ------------------------------------------------------------
-# $2: Group_ID | $3: Group
+ elif [ $1 = "DEL_USER_HOME" ]
+  then
+   clear;
+	rm -r /home/$2;
+
+
+# ------------------------------------------------------------
+ elif [ $1 = "DEL_GROUP" ]
+  then
+   clear;
+    delgroup $2;
+
+
+# ------------------------------------------------------------
  elif [ $1 = "ADD_GROUP" ]
   then
    clear;
-   groupadd -g $2 $3;
+    addgroup $2 --gid $3 ;
 
 
 # ------------------------------------------------------------
-# $2 = USER | $3 USER_ID | $4 GROUP_ID
  elif [ $1 = "ADD_USER" ]
   then
    clear;
-   useradd --home-dir /home/$2 \
-   --base-dir /home/$2 \
-   --uid $3 \
-   --gid $4 \
-   --groups sudo \
-   --no-user-group \
-   --shell /bin/bash \
-   --create-home /home/$2;
-
+    useradd \
+      --home-dir /home/$2 \
+      --base-dir /home/$2 \
+      --uid $3 \
+      --gid $4 \
+      --groups $5 \
+      --no-user-group \
+      --shell /bin/bash \
+      --create-home $2;
 
 # ------------------------------------------------------------
-# $2: USER | $3: PASS
- elif [ $1 = "PASS" ]
+ elif [ $1 = "DEL_USER_GROUP" ]
   then
    clear;
-   echo "$2:$3" | chpasswd $2;
+    deluser $2 $3;
 
 # ------------------------------------------------------------
-# $2: USER
- elif [ $1 = "ROOT" ]
+ elif [ $1 = "ADD_USER_GROUP" ]
   then
-   clear
+   clear;
+    adduser $2 $3;
+
+# ------------------------------------------------------------
+ elif [ $1 = "PASSWORD" ]
+  then
+   clear;
+    echo "$2:$3" | chpasswd ;
+
+# ------------------------------------------------------------
+ elif [ $1 = "ADD_SUDOER" ]
+  then
+   clear;
     echo "$2 ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/$2;
 
 
 # ------------------------------------------------------------
-# $2: USER | $3 Group
+ elif [ $1 = "DEL_SUDOER" ]
+  then
+   clear;
+	rm /etc/sudoers.d/$2;
+
+
+# ------------------------------------------------------------
  elif [ $1 = "CHECK" ]
   then
    clear;
-    echo "-------------------"
-    cat /etc/passwd | grep $2;
-    echo "-------------------"
-    cat /etc/group  | grep $3;
-    echo "-------------------"
-    cat /etc/sudoers.d/$2;
-    echo "-------------------"
-    id $2;
+    id $2; groups $2;
+	cat /etc/sudoers.d/$2;
+
 
 # ------------------------------------------------------------
  else
