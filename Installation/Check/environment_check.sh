df157f50f2500119677e7a991601a8ac65eb9539
Marc <teste74@hotmail.fr>
2021-12-19 09:20:37 +0100

    Update environment_check.sh

--- Installation/Check/environment_check.sh
+++ Installation/Check/environment_check.sh
@@ -17,8 +17,8 @@ echo "# - VILLE          : '$VILLE'"
 echo "# - LANGUE         : '$LANGUE'"
 echo "#"
 echo "# ----------------------------------------"
-echo "# - Interfaces     : '$(ifconfig ens18 | grep "ens18" | cut -c 1-5  )'"
-echo "# - Adresse IP     : '$(ifconfig ens18 | grep "inet " | cut -c 14-25)'"
+echo "# - Interfaces     : '$(ip netconf | grep "inet ens18" | cut -c 6-10)'"
+echo "# - Adresse IP     : '$(ip -br ad | grep ens18 | cut -d / -f 1 | cut -c 33-45)'"
 echo "# - Passerelle     : '$NET_GATEWAY'"
 echo "# - Serveur DNS    : '$NET_DNS0'"
 echo "#                  : '$NET_DNS1'"
@@ -28,6 +28,3 @@ echo "# - UNIX  - USER   : [$USER_ID] $USER:$PASS"
 echo "# - UNIX  - Groupe : [$GROUP_UID] $GROUP"
 echo "# - SAMBA - COmpte : [XXXX] $SAMBA_USER:$SAMBA_PASS"
 echo "################################################"
-
-
-# Requis: net-tools (ifconfig)
