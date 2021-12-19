c07c913a8505be23f68df9eb56d72d1a1091c9b7
Marc <teste74@hotmail.fr>
2021-12-19 01:32:17 +0100

    Update ADGuardHome.sh

--- APPZ/ADGuardHome.sh
+++ APPZ/ADGuardHome.sh
@@ -35,20 +35,24 @@ SERVICE()
 # - uninstall
 
 
-
+# ------------------------------------------------------------------------------------------------------------------------------------
 if [ -z $1 ]
  then
   echo 'Merci de préciser un paramètre (PURGE, DL, ENABLE, DISABLE, START, STOP)'
 # --------------------------------------------
+
  elif [ "$1" = "PURGE" ]
   then
    /opt/AdGuardHome/AdGuardHome -s uninstall;
    rm -r /opt/AdGuardHome;
 # --------------------------------------------
+
 elif [ "$1" = "DL" ]
   then
-   echo "Lancement: DL"
+   wget https://static.adguard.com/adguardhome/release/AdGuardHome_linux_amd64.tar.gz -O /tmp/AdGuardHome_linux_amd64.tar.gz ;
+   tar -xf /tmp/AdGuardHome_linux_amd64.tar.gz -C /opt;
 # --------------------------------------------
+
  elif [ "$1" = "ENABLE" ]
   then
    /opt/AdGuardHome/AdGuardHome -s install
@@ -67,3 +71,4 @@ elif [ "$1" = "DL" ]
  else
   echo 'Le paramètre $1 est inconnu'
 fi
+# ------------------------------------------------------------------------------------------------------------------------------------
