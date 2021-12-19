ea4e083c1ca4d8e117a28aaa52119c99eb4b51a2
Marc <teste74@hotmail.fr>
2021-12-19 17:13:00 +0100

    Update SSH.sh

--- Installation/CONFIG/SSH.sh
+++ Installation/CONFIG/SSH.sh
@@ -17,14 +17,14 @@ if [ -z $1 ]
   - STEP_7: Relance du service
   "
 
-# ----------------------------------------------------------
+# -------------------------------------------------------------------------------------------
 # Création des dossiers SSH
  elif [ $1 = "STEP_1" ]
   then
    clear;
    /sbin/runuser -l root  -c 'mkdir -p /root/.ssh'
    /sbin/runuser -l $USER -c 'mkdir /home/$USER/.ssh' ;
-# ----------------------------------------------------------
+# -------------------------------------------------------------------------------------------
 # Insertion de la Clé SSH sur le serveur
  elif [ $1 = "STEP_2" ] 
   then
@@ -32,7 +32,7 @@ if [ -z $1 ]
    /sbin/runuser -l root  -c "echo '$SSH_KEY' > /root/.ssh/authorized_keys" ;
    /sbin/runuser -l $USER -c "echo '$SSH_KEY' > /home/$USER/.ssh/authorized_keys" ;
 
-# ----------------------------------------------------------
+# -------------------------------------------------------------------------------------------
 # Modification des permissions
  elif [ $1 = "STEP_3" ]
   then
@@ -43,20 +43,20 @@ if [ -z $1 ]
    #User:
    chmod 700 /home/$USER/.ssh ;
    chmod 600 /home/$USER/.ssh/authorized_keys ;
-# ----------------------------------------------------------
+# -------------------------------------------------------------------------------------------
 # Backup
  elif [ $1 = "STEP_4" ]
   then
    clear;
    cp /etc/ssh/sshd_config /etc/ssh/sshd_config.old;
 
-# ----------------------------------------------------------
+# -------------------------------------------------------------------------------------------
 # Recovery
  elif [ $1 = "STEP_5" ]
   then
    clear;
    cp /etc/ssh/sshd_config.old /etc/ssh/sshd_config;
-# ----------------------------------------------------------
+# -------------------------------------------------------------------------------------------
 # Sécurisation du SSH
  elif [ $1 = "STEP_6" ]
   then
@@ -64,10 +64,10 @@ if [ -z $1 ]
    # Limiter l'accès SSH au Groupe / USER suivant:
    echo "AllowUsers $USER root" >>                                     /etc/ssh/sshd_config;
    echo "AllowGroups $GROUP"    >>                                     /etc/ssh/sshd_config;
-   
+
    # Fichier contenant les Clés SSH
    sed -i 's/#AuthorizedKeysFile/AuthorizedKeysFile/g'                 /etc/ssh/sshd_config;
-   
+
    # Fermer les connexions Zombie
    echo "KeepAlive no"          >>                                     /etc/ssh/sshd_config;
 
@@ -79,50 +79,39 @@ if [ -z $1 ]
 
    # Authentification par Mot de passe (Yes à NO)
    sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g'  /etc/ssh/sshd_config;
-   
+
    # Autoriser Connexion sans Mot de passe
    sed -i 's/#PermitEmptyPasswords no/PermitEmptyPasswords no/g'       /etc/ssh/sshd_config;
-   
+
    # Authentification du Root
    sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config;
-   
+
    # Définir emplacement PID
    sed -i 's/#PidFile/PidFile/g'                                       /etc/ssh/sshd_config;
-   
+
    # Changer le port d'écouter SSH
    sed -i 's/#Port 22/Port 22/g'                                       /etc/ssh/sshd_config;
-   
+
    # Afficher la Dernière Connexion
    sed -i 's/#PrintLastLog yes/PrintLastLog yes/g'                     /etc/ssh/sshd_config;
 
    # Authentication par clé SSH
    sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g'     /etc/ssh/sshd_config;
-   
+
    # Vérifier Permissions Home à la connexion
    sed -i 's/#StrictModes yes/StrictModes yes/g'                       /etc/ssh/sshd_config;
-   
-   echo "Sécurisation Terminé";
 
-# ----------------------------------------------------------
+   echo "Sécurisation Terminé";
+# -------------------------------------------------------------------------------------------
 # Relance du service
  elif [ $1 = "STEP_7" ]
   then
    clear;
    systemctl restart ssh;
-# ----------------------------------------------------------
+# -------------------------------------------------------------------------------------------
  else
   echo "Script KO"
 fi
-
-
-# Recovery
-#clear; cp /etc/ssh/sshd_config.old /etc/ssh/sshd_config;
-
-
-
-# Relancer le service
-#systemctl restart ssh;
-
 
 
 # ChallengeResponseAuthentication
