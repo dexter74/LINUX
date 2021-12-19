c7c7cdb5f9012e4a009ee43ec7e13235adfc18a0
Marc <teste74@hotmail.fr>
2021-12-19 16:59:10 +0100

    Update 3.Paquets.sh

--- Installation/CONFIG/3.Paquets.sh
+++ Installation/CONFIG/3.Paquets.sh
@@ -3,48 +3,49 @@
 ####################################
 
 if [ -z $1 ]
-# ------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------
  then
   clear;
   echo "Merci d'indiquer un argument"
-# ------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------
  elif [ $1 = "CDROM" ]
   then
    clear;
    sed -i 's/^deb cdrom/#deb cdrom/g' /etc/apt/sources.list;
-# ------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------
  elif [ $1 = "BACKPORT" ]
   then
    clear;
    echo "deb http://ftp.de.debian.org/debian buster main" > /etc/apt/sources.list\backport.list;
-# ------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------
  elif [ $1 = "MAJ" ]
   then
    clear;
    apt update  -qq     1>/dev/NULL 2>/dev/NULL;
    apt upgrade -qq -y  1>/dev/NULL 2>/dev/NULL;
-# ------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------
  elif [ $1 = "FIX" ]
   then
    apt update --fix-missing -y 1>/dev/NULL 2>/dev/NULL;
    dpkg --configure -a 1>/dev/NULL 2>/dev/NULL;
-# ------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------
  elif [ $1 = "BASE" ]
   then
    clear;
-   apt install -y -qq bash-completion curl debconf-utils dnsutils git gnupg net-tools sudo unzip wget 1>/dev/NULL 2>/dev/NULL;
-# ------------------------------------------------------------
+   apt install -y -qq bash-completion curl debconf-utils \
+   dnsutils git gnupg net-tools sudo unzip wget 1>/dev/NULL 2>/dev/NULL;
+# -----------------------------------------------------------------------------------------------
  elif [ $1 = "OUTIL" ]
   then
    clear;
    apt install -y -qq cifs-utils ntfs-3g open-iscsi 1>/dev/NULL 2>/dev/NULL;
-# ------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------
  elif [ $1 = "QEMU" ]
   then
    clear;
    apt install -y -qq qemu-guest-agent 1>/dev/NULL 2>/dev/NULL;
 
-# ------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------
  elif [ $1 = "SAMBA" ]
   then
    clear;
@@ -52,14 +53,14 @@ if [ -z $1 ]
    echo "samba-common samba-common/dhcp boolean false" | debconf-set-selections;
    echo "samba-common samba-common/do_debconf boolean true" | debconf-set-selections;
    apt install -y -qq samba smbclient 1>/dev/NULL 2>/dev/NULL;
-# ------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------
  elif [ $1 = "KERNEL" ]
   then
    clear;
    apt install -t -y buster-backports 1>/dev/NULL 2>/dev/NULL;;
    apt install -t -y linux-image-amd64 1>/dev/NULL 2>/dev/NULL;;
    apt install -t -y firmware-linux 1>/dev/NULL 2>/dev/NULL;;
-# ------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------
  elif [ $1 = "SERVICE_OFF" ]
   then
    clear;
@@ -69,7 +70,7 @@ if [ -z $1 ]
    systemctl stop smbd;
    systemctl stop iscsid.service;
    systemctl stop open-iscsi.service;
-# ------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------
  else
   echo "Script KO"
 fi
