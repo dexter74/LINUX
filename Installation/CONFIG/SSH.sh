d8af27ef113d22cf93522197a87829080c850170
Marc <teste74@hotmail.fr>
2021-12-19 14:23:06 +0100

    Update SSH.sh

--- Installation/CONFIG/SSH.sh
+++ Installation/CONFIG/SSH.sh
@@ -18,19 +18,22 @@ if [ -z $1 ]
   "
 
 # ----------------------------------------------------------
+# Création des dossiers SSH
  elif [ $1 = "STEP_1" ]
   then
    clear;
    /sbin/runuser -l root  -c 'mkdir -p /root/.ssh'
    /sbin/runuser -l $USER -c 'mkdir /home/$USER/.ssh' ;
 # ----------------------------------------------------------
- elif [ $1 = "STEP_2" ]
+# Insertion de la Clé SSH sur le serveur
+ elif [ $1 = "STEP_2" ] 
   then
    clear;
    /sbin/runuser -l root  -c "echo '$SSH_KEY' > /root/.ssh/authorized_keys" ;
    /sbin/runuser -l $USER -c "echo '$SSH_KEY' > /home/$USER/.ssh/authorized_keys" ;
 
 # ----------------------------------------------------------
+# Modification des permissions
  elif [ $1 = "STEP_3" ]
   then
    clear;
@@ -41,41 +44,52 @@ if [ -z $1 ]
    chmod 700 /home/$USER/.ssh ;
    chmod 600 /home/$USER/.ssh/authorized_keys ;
 # ----------------------------------------------------------
+# Backup
  elif [ $1 = "STEP_4" ]
-  then
-   clear;
-# ----------------------------------------------------------
- elif [ $1 = "STEP_5" ]
-  then
-   clear;
-   systemctl restart ssh ;
-   systemctl restart sshd ;
-
-# ----------------------------------------------------------
- elif [ $1 = "STEP_6" ]
   then
    clear;
    cp /etc/ssh/sshd_config /etc/ssh/sshd_config.old;
 
 # ----------------------------------------------------------
+# Recovery
+ elif [ $1 = "STEP_5" ]
+  then
+   clear;
+   cp /etc/ssh/sshd_config.old /etc/ssh/sshd_config;
+# ----------------------------------------------------------
+# Sécurisation du SSH
+ elif [ $1 = "STEP_6" ]
+  then
+   clear;
+
+# ----------------------------------------------------------
+# Relance du service
  elif [ $1 = "STEP_7" ]
   then
    clear;
-    cp /etc/ssh/sshd_config.old /etc/ssh/sshd_config;
-
-
-# ----------------------------------------------------------
- elif [ $1 = "RESET" ]
-  then
-   clear;
-   sed -i 's/AuthorizedKeysFile/#AuthorizedKeysFile/g'                /etc/ssh/sshd_config ;
-   sed -i 's/PasswordAuthentication yes/#PasswordAuthentication no/g' /etc/ssh/sshd_config ;
-   sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config ;
-   sed -i 's/PermitRootLogin no/PermitRootLogin yes/g'                /etc/ssh/sshd_config ;
-   sed -i 's/PubkeyAuthentication yes/#PubkeyAuthentication no/g'     /etc/ssh/sshd_config ;
-   sed -i 's/PidFile/#PidFile/g'                                      /etc/ssh/sshd_config ;
-   sed -i 's/PrintLastLog/#PrintLastLog /g'                           /etc/ssh/sshd_config ;
+   systemctl restart ssh;
+   systemctl restart sshd;
 # ----------------------------------------------------------
  else
   echo "Script KO"
 fi
+
+
+#Défaut
+#AuthorizedKeysFile     .ssh/authorized_keys .ssh/authorized_keys2
+#PasswordAuthentication yes
+PermitRootLogin prohibit-password
+#PidFile /var/run/sshd.pid
+#PrintLastLog yes
+#PubkeyAuthentication yes
+
+#Sécurisation
+sed -i 's/#AuthorizedKeysFile/AuthorizedKeysFile /g' /etc/ssh/sshd_config ;
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config ;
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/g' /etc/ssh/sshd_config ;
+sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config ;
+sed -i 's/#PidFile/PidFile /g' /etc/ssh/sshd_config ;
+sed -i 's/#PrintLastLog/PrintLastLog /g' /etc/ssh/sshd_config ;
+sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/g' /etc/ssh/sshd_config ;
+
+
