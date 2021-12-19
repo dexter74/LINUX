1fab6af8377049da8950afd813122a885e794a18
Marc <teste74@hotmail.fr>
2021-12-19 09:51:35 +0100

    Update 2.reseau.sh

--- Installation/CONFIG/2.reseau.sh
+++ Installation/CONFIG/2.reseau.sh
@@ -11,11 +11,18 @@ echo "net.ipv6.conf.all.disable_ipv6 = 1" > /etc/sysctl.d/70-disable-ipv6.conf ;
 #####################################################
 # Configuration des Interfaces Réseaux en mode DHCP #
 #####################################################
-echo "# Interface de bouclage
+if [ "$1" = "DHCP" ]
+ then
+  clear;
+  echo "# Interface de bouclage
 auto $NET_LOOPBACK
 iface $NET_LOOPBACK inet loopback
 #
 # Interface Physique
 auto $NET_INTERFACE1
 allow-hotplug $NET_INTERFACE1
-iface $NET_INTERFACE1 inet dhcp" > /etc/network/interfaces ;
+iface $NET_INTERFACE1 inet dhcp" > /etc/network/interfaces;
+cat  /etc/network/interfaces ;
+ else
+  echo "Merci d'indiquer un parametre: DHCP, STATIC"
+fi
