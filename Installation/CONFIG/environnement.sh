797e1f98ec1b56072db6259ce5ce97570b31b821
Marc <teste74@hotmail.fr>
2021-12-19 08:36:40 +0100

    Update environnement.sh

--- Installation/CONFIG/environnement.sh
+++ Installation/CONFIG/environnement.sh
@@ -7,44 +7,42 @@
 # test.sh <DISTRIB> <release> <hostname> <domaine> <region> <ville> <langue> <IPV4> <Passerelle> <DNS 0> <DNS 1> <USERNAME> <GROUP> <PASSWOD> <USER ID> <GROUP ID> <SAMBA USER> <SAMBA PASS>
 ######################################################################################################################################################################################################
 
-# rm test.sh; nano test.sh; sh test.sh;
+#Fichier Profil:
+SYSOUT=/etc/profile.d/ENVIRONNEMENT.sh
 
-echo VALEUR=\'$1\' > RESULT
 
-
-clear;
-#Fichier Profil: /etc/profile.d/ENVIRONNEMENT.sh
-SYSOUT=out
-echo "" > $SYSOUT
-echo ############################## >> out
-echo # Information sur le Système # >> out
-echo ############################## >> out
-echo export DISTRIB=\'$1\' >> out
-echo export RELEASE=\'$2\' >> out
-echo export HOSTNAME=\'$3\' >> out
-echo export DOMAINE=\'$4\' >> out
-echo export REGION=\'$5\' >> out
-echo export VILLE=\'$6\' >> out
-echo export LANGUE=\'$7\' >> out
-echo export SSH_KEY=\'MAKEY\' >> out
-echo ############################ >> out
-echo # Information sur le Rseau # >> out
-echo ############################ >> out
-echo export NET_LOOPBACK=\'$(ip link   | grep '^1:' | cut -c 4-20 |cut -d ':' -f 1)\' >> out
-echo export NET_INTERFACE1=\'$(ip link | grep '^2:' | cut -c 4-20 |cut -d ':' -f 1)\' >> out
-echo export NET_INTERFACE2=\'$(ip link | grep '^3:' | cut -c 4-20 |cut -d ':' -f 1)\' >> out
-echo export NET_ADDRESS=\'$8\' >> out
-echo export NET_GATEWAY=\'$9\' >> out
-echo export NET_DNS0=\'$10\' >> out
-echo export NET_DNS1=\'$11\' >> out
-echo ############################ >> out
-echo # Information sur le Rseau # >> out
-echo ############################ >> out
-echo export USER=\'$12\' >> out
-echo export GROUP=\'$13\' >> out
-echo export PASS=\'$14\' >> out
-echo export USER_ID=\'$15\' >> out
-echo export GROUP_UID=\'$16\' >> out
-echo export APPZ_SAMBA_USER=\'$17\' >> out
-echo export APPZ_SAMBA_PASS=\'$18\' >> out
-
+#Mise à Blank du Fichier SYSOUT
+echo '' > $SYSOUT
+echo '##############################' >> $SYSOUT
+echo '# Information sur le Système #' >> $SYSOUT
+echo '##############################' >> $SYSOUT
+echo export DISTRIB=\'$1\' >> $SYSOUT
+echo export RELEASE=\'$2\' >> $SYSOUT
+echo export HOSTNAME=\'$3\' >> $SYSOUT
+echo export DOMAINE=\'$4\' >> $SYSOUT
+echo export REGION=\'$5\' >> $SYSOUT
+echo export VILLE=\'$6\' >> $SYSOUT
+echo export LANGUE=\'$7\' >> $SYSOUT
+echo export SSH_KEY=\'MAKEY\' >> $SYSOUT
+echo '############################' >> $SYSOUT
+echo '# Information sur le Rseau #' >> $SYSOUT
+echo '############################' >> $SYSOUT
+echo export NET_LOOPBACK=\'$(ip link   | grep '^1:' | cut -c 4-20 |cut -d ':' -f 1)\' >> $SYSOUT
+echo export NET_INTERFACE1=\'$(ip link | grep '^2:' | cut -c 4-20 |cut -d ':' -f 1)\' >> $SYSOUT
+echo export NET_INTERFACE2=\'$(ip link | grep '^3:' | cut -c 4-20 |cut -d ':' -f 1)\' >> $SYSOUT
+echo export NET_ADDRESS=\'$8\' >> $SYSOUT
+echo export NET_GATEWAY=\'$9\' >> $SYSOUT
+echo export NET_DNS0=\'$10\' >> $SYSOUT
+echo export NET_DNS1=\'$11\' >> $SYSOUT
+echo '############################' >> $SYSOUT
+echo '# Information sur le Rseau #' >> $SYSOUT
+echo '############################' >> $SYSOUT
+echo export USER=\'$12\' >> $SYSOUT
+echo export GROUP=\'$13\' >> $SYSOUT
+echo export PASS=\'$14\' >> $SYSOUT
+echo export USER_ID=\'$15\' >> $SYSOUT
+echo export GROUP_UID=\'$16\' >> $SYSOUT
+echo export APPZ_SAMBA_USER=\'$17\' >> $SYSOUT
+echo export APPZ_SAMBA_PASS=\'$18\' >> $SYSOUT
+clear ;
+cat $SYSOUT
