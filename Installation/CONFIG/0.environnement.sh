880f3a530e01e2386cc933db3eb18be0b9835c8b
Marc <teste74@hotmail.fr>
2021-12-19 16:23:27 +0100

    Update 0.environnement.sh

--- Installation/CONFIG/0.environnement.sh
+++ Installation/CONFIG/0.environnement.sh
@@ -22,7 +22,7 @@ echo export REGION=\'$5\' >> $SYSOUT
 echo export VILLE=\'$6\' >> $SYSOUT
 echo export LANGUE=\'$7\' >> $SYSOUT
 echo export SSH_KEY=\'ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAs+2hvOiFJvMh7Tdmle+bAYLwEyErLFYE74ljUny/2D2zl6XTZmBw7JEdD3H9mXqeJTk/BGEa2Sn4G0OkwrYH6xnXlwsLRB1Wosa2Q2vX1vG1MxU6e/t7ngsQ74wHGj6Nq5hNZP7EfTfTA6Jo26k9BU5+qj1C4Oq9uvurZuwVl9MYVX59CQb5jKIqgB/obllICqNaGk+msxDSEKh2gFzcEHLa0Kf8R6t+z1m79M+X5I0SLosQluc4edfj77Y3OqPNBooDt5i3MqAJWvzagGStQN2RSs3LUPaMSMleew2qKZEHRzl9u75MaAtKIpH7qe4xHPprDXAF1bKIa2PTNRv9HQ== teste74@hotmail.fr\' >> $SYSOUT
-
+echo '#' >> $SYSOUT
 echo '#############################' >> $SYSOUT
 echo '# Information sur le Reseau #' >> $SYSOUT
 echo '#############################' >> $SYSOUT
@@ -33,6 +33,7 @@ echo export NET_ADDRESS=\'$8\' >> $SYSOUT
 echo export NET_GATEWAY=\'$9\' >> $SYSOUT
 echo export NET_DNS0=\'$10\' >> $SYSOUT
 echo export NET_DNS1=\'$11\' >> $SYSOUT
+echo '#' >> $SYSOUT
 echo '############################' >> $SYSOUT
 echo '# Information sur les USER #' >> $SYSOUT
 echo '############################' >> $SYSOUT
@@ -41,13 +42,12 @@ echo export PASS=\'$14\' >> $SYSOUT
 echo export GROUP=\'$13\' >> $SYSOUT
 echo export USER_ID=\'$15\' >> $SYSOUT
 echo export GROUP_UID=\'$16\' >> $SYSOUT
+echo '#' >> $SYSOUT
 echo '##########################' >> $SYSOUT
 echo '# Configuration des APPZ #' >> $SYSOUT
 echo '##########################' >> $SYSOUT
 echo export SAMBA_USER=\'$17\' >> $SYSOUT
 echo export SAMBA_PASS=\'$18\' >> $SYSOUT
-
-
 
 
 ########################
