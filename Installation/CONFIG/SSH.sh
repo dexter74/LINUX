c935e7f79c02e4562e83f7c5114e6b731a374c90
Marc <teste74@hotmail.fr>
2021-12-19 13:43:19 +0100

    Update SSH.sh

--- Installation/CONFIG/SSH.sh
+++ Installation/CONFIG/SSH.sh
@@ -6,7 +6,8 @@
 if [ -z $1 ]
 # ----------------------------------------------------------
  then
-  echo "
+  clear;
+  echo "Argument de commande manquant.
   - Step 1: Création des dossiers SSH
   - Step 2: Insertion de la Clé SSH sur le serveur
   - Step 3: Modification des permissions
@@ -17,25 +18,29 @@ if [ -z $1 ]
 # ----------------------------------------------------------
  elif [ $1 = "STEP_1" ]
   then
+   clear;
    echo ""
 # ----------------------------------------------------------
  elif [ $1 = "STEP_2" ]
   then
+   clear;
    echo ""
 # ----------------------------------------------------------
  elif [ $1 = "STEP_3" ]
   then
+   clear;
    echo ""
 # ----------------------------------------------------------
  elif [ $1 = "STEP_4" ]
   then
+   clear;
    echo ""
 # ----------------------------------------------------------
  elif [ $1 = "STEP_5" ]
   then
+   clear;
    echo ""
 # ----------------------------------------------------------
-
  else
   echo "Script KO"
 fi
