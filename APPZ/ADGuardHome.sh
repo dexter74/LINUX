f4848aea547974394b6a0638c837f42dc68228a9
Marc <teste74@hotmail.fr>
2021-12-19 01:13:05 +0100

    Update ADGuardHome.sh

--- APPZ/ADGuardHome.sh
+++ APPZ/ADGuardHome.sh
@@ -1,22 +1,8 @@
-###########################
-# Déploiement ADGuardHOme #
-###########################
+################################################
+# ./ADGuardHome.sh <CLEAN> <DOWNLOAD> <ACTION> #
+################################################
+clear;
 
-# sh ./ADGuardHome.sh <CLEAN> <DOWNLOAD> <ACTION>
-
-
-
-CLEAN=$1
-DOWNLOAD=$2
-ACTION=$3
-
-
-# Etat du service:
-# - install
-# - status
-# - start
-# - stop
-# - uninstall
 
 
 ################################################################################################################################
@@ -36,30 +22,19 @@ DOWNLOAD()
 ################################################################################################################################
 SERVICE()
  {
- /opt/AdGuardHome/AdGuardHome -s $3;
+ /opt/AdGuardHome/AdGuardHome -s ??;
  }
 
 ################################################################################################################################
 
 
-if [ "$CLEAN" == "1" ]
- then
-  echo "Condition CLEAN OK"
- else
-  echo "Condition CLEAN KO"
-fi
 
-
-if [ "$DOWNLOAD" == "1" ]
- then
-  echo "Condition Download OK"
- else
-  echo "Condition Download KO"
-fi
-
-if [ "$ACTION" == "1" ]
- then
-echo "Condition Action $3"
- else
-  echo "Condition Action KO"
-fi
+###########################
+# Déclaration de Variable #
+###########################
+# Etat du service:
+# - install
+# - status
+# - start
+# - stop
+# - uninstall
