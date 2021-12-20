7513c1b3c5976d4e5299b51a6ba68368b6fd54f9
Marc <teste74@hotmail.fr>
2021-12-21 00:02:19 +0100

    Update Docker.sh

--- DEV/APPZ/Docker.sh
+++ DEV/APPZ/Docker.sh
@@ -1,6 +1,8 @@
-###############
-# ./Docker.sh #
-###############
+#################################################
+# Script de de déploiement de Docker sur Debian #
+#################################################
+
+# Nettoyage de la console
 clear;
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -8,24 +10,25 @@ if [ -z $1 ]
  then
   echo 'Merci de preciser un parametre (PURGE, PREP, INSTALL' 
 # -------------------------------------------------------------------------------------------------------------------------------------
+#  1>/dev/null 2>/dev/null;
  elif [ "$1" = "PURGE" ]
   then
-   apt autoremove --purge -y docker-ce docker-ce-cli containerd.io ;
-   rm -rf /var/lib/docker ;
-   rm -rf /var/lib/containerd ;
-   rm -rf /etc/docker ;
-   rm -rf /usr/local/bin/docker-compose ;
+   apt autoremove --purge -y docker-ce docker-ce-cli containerd.io;
+   rm -rf /var/lib/docker;
+   rm -rf /var/lib/containerd;
+   rm -rf /etc/docker;
+   rm -rf /usr/local/bin/docker-compose;
 # -------------------------------------------------------------------------------------------------------------------------------------
  elif [ "$1" = "PREP" ]
   then
-  apt update 1>/dev/null 2>/dev/null;
-  apt install -y apt-transport-https ca-certificates gnupg-agent gnupg2 software-properties-common sudo curl 1>/dev/null 2>/dev/null;
+  apt update;
+  apt install -y apt-transport-https ca-certificates gnupg-agent gnupg2 software-properties-common sudo curl;
   curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -;
   add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $RELEASE stable";
 # -------------------------------------------------------------------------------------------------------------------------------------
  elif [ "$1" = "INSTALL" ]
   then
-  apt install -y docker-ce docker-ce-cli containerd.io 1>/dev/null 2>/dev/null;
+  apt install -y docker-ce docker-ce-cli containerd.io;
 # -------------------------------------------------------------------------------------------------------------------------------------
  else
   echo "Le parametre '$1' est inconnu"
