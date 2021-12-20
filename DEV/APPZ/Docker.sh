e05d006923a71f5ce5b785671aeeccf4dced59d0
Marc <teste74@hotmail.fr>
2021-12-21 00:14:51 +0100

    Update Docker.sh

--- DEV/APPZ/Docker.sh
+++ DEV/APPZ/Docker.sh
@@ -12,7 +12,13 @@ if [ -z $1 ]
   echo 'Merci de preciser un parametre
   - PURGE
   - PREP 
-  - INSTALL'
+  - INSTALL
+  - HUB <Login> <PASS>
+  - TEST (Lance le conteneur Hello-World)
+  - DOCKER_CLEAN (Kill + Clean conteneurs / images)
+  - VERSION (Docker, Docker-Compose)
+  '
+
 # -------------------------------------------------------------------------------------------------------------------------------------
 #  1>/dev/null 2>/dev/null;
  elif [ "$1" = "PURGE" ]
@@ -23,7 +29,9 @@ if [ -z $1 ]
    rm -rf /var/lib/containerd;
    rm -rf /etc/docker;
    rm -rf /usr/local/bin/docker-compose;
+
 # -------------------------------------------------------------------------------------------------------------------------------------
+# Mise à jour Dépôt, Installation des pré-requis et ajout du dépôts docker.
  elif [ "$1" = "PREP" ]
   then
   clear;
@@ -31,12 +39,51 @@ if [ -z $1 ]
   apt install -qq -y apt-transport-https ca-certificates gnupg-agent gnupg2 software-properties-common sudo curl;
   curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -;
   add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $RELEASE stable";
+
 # -------------------------------------------------------------------------------------------------------------------------------------
+# Install Docker avec Docker Compose
  elif [ "$1" = "INSTALL" ]
   then
+  clear;
   apt install -qq -y docker-ce docker-ce-cli containerd.io;
+  curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose ;
+  chmod +x /usr/local/bin/docker-compose ;
+  ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose ;
+
+# -------------------------------------------------------------------------------------------------------------------------------------
+#  COnnexion au Docker-Hub ($2: login | $3: Password)
+ elif [ "$1" = "HUB" ]
+  then
+  clear;
+  docker login -u $2 -p $3;
+
+# -------------------------------------------------------------------------------------------------------------------------------------
+# Install Docker avec Docker Compose
+ elif [ "$1" = "TEST" ]
+  then
+  clear;
+  docker run hello-world;
+
+# -------------------------------------------------------------------------------------------------------------------------------------
+# Tue les conteneurs, purge les conteneurs leurs images. (Volumes intact)
+ elif [ "$1" = "DOCKER_CLEAN" ]
+  then
+  clear;
+  docker kill $(docker ps -q) ;
+  docker rm $(docker ps -a -q) ;
+  docker rmi $(docker images -q) ;
+
+# -------------------------------------------------------------------------------------------------------------------------------------
+# Install Docker avec Docker Compose
+ elif [ "$1" = "VERSION" ]
+  then
+  clear;
+  docker --version;
+  docker-compose --version;
+
 # -------------------------------------------------------------------------------------------------------------------------------------
  else
   echo "Le parametre '$1' est inconnu"
+# -------------------------------------------------------------------------------------------------------------------------------------
 fi
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
