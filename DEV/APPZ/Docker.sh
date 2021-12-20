a9ac9d4af49d65d7e114d95d3b5e1f60c7647790
Marc <teste74@hotmail.fr>
2021-12-21 00:04:18 +0100

    Update Docker.sh

--- DEV/APPZ/Docker.sh
+++ DEV/APPZ/Docker.sh
@@ -8,6 +8,7 @@ clear;
 # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 if [ -z $1 ]
  then
+  clear;
   echo 'Merci de preciser un parametre
   - PURGE
   - PREP 
@@ -16,6 +17,7 @@ if [ -z $1 ]
 #  1>/dev/null 2>/dev/null;
  elif [ "$1" = "PURGE" ]
   then
+   clear;
    apt autoremove --purge -y docker-ce docker-ce-cli containerd.io;
    rm -rf /var/lib/docker;
    rm -rf /var/lib/containerd;
@@ -24,14 +26,15 @@ if [ -z $1 ]
 # -------------------------------------------------------------------------------------------------------------------------------------
  elif [ "$1" = "PREP" ]
   then
-  apt update;
-  apt install -y apt-transport-https ca-certificates gnupg-agent gnupg2 software-properties-common sudo curl;
+  clear;
+  apt update ;
+  apt install -qq -y apt-transport-https ca-certificates gnupg-agent gnupg2 software-properties-common sudo curl;
   curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -;
   add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $RELEASE stable";
 # -------------------------------------------------------------------------------------------------------------------------------------
  elif [ "$1" = "INSTALL" ]
   then
-  apt install -y docker-ce docker-ce-cli containerd.io;
+  apt install -qq -y docker-ce docker-ce-cli containerd.io;
 # -------------------------------------------------------------------------------------------------------------------------------------
  else
   echo "Le parametre '$1' est inconnu"
