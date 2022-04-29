fdf96588a97d50fdb33076bb3a507570152de59a
Marc <teste74@hotmail.fr>
2022-04-30 01:49:52 +0200

    Update 3.paquets.sh

--- Installation/CONFIG/3.paquets.sh
+++ Installation/CONFIG/3.paquets.sh
@@ -19,55 +19,47 @@ if [ -z $1 ]
   - 3.paquets.sh SERVICE_ON
   "
 # -----------------------------------------------------------------------------------------------
- elif [ $1 = "CDROM" ]
-  then
+ elif [ $1 = "CDROM" ]; then
    clear;
    sed -i 's/^deb cdrom/#deb cdrom/g' /etc/apt/sources.list;
    grep 'cdrom' /etc/apt/sources.list;
 
 # -----------------------------------------------------------------------------------------------
- elif [ $1 = "BACKPORT" ]
-  then
+ elif [ $1 = "BACKPORT" ]; then
    clear;
    echo "deb http://ftp.de.debian.org/debian $RELEASE main" > /etc/apt/sources.list.d/backport.list;
    cat /etc/apt/sources.list.d/backport.list;
    
 # -----------------------------------------------------------------------------------------------
- elif [ $1 = "MAJ" ]
-  then
+ elif [ $1 = "MAJ" ]; then
    clear;
    apt update  -qq;
    apt upgrade -qq -y;
    #1>/dev/NULL 2>/dev/NULL;
 
 # -----------------------------------------------------------------------------------------------
- elif [ $1 = "FIX" ]
-  then
+ elif [ $1 = "FIX" ]; then
    apt update --fix-missing -y;
    dpkg --configure -a;
 
 # -----------------------------------------------------------------------------------------------
- elif [ $1 = "BASE" ]
-  then
+ elif [ $1 = "BASE" ]; then
    clear;
    apt install -y -qq bash-completion curl debconf-utils \
    dnsutils git gnupg net-tools sudo unzip wget;
 
 # -----------------------------------------------------------------------------------------------
- elif [ $1 = "OUTIL" ]
-  then
+ elif [ $1 = "OUTIL" ]; then
    clear;
    apt install -y -qq cifs-utils ntfs-3g open-iscsi;
 
 # -----------------------------------------------------------------------------------------------
- elif [ $1 = "QEMU" ]
-  then
+ elif [ $1 = "QEMU" ]; then
    clear;
    apt install -y -qq qemu-guest-agent;
 
 # -----------------------------------------------------------------------------------------------
-# elif [ $1 = "SAMBA" ]
-#  then
+# elif [ $1 = "SAMBA" ]; then
 #   clear;
 #   echo "samba-common samba-common/workgroup string WORKGROUP" | debconf-set-selections;
 #   echo "samba-common samba-common/dhcp boolean false" | debconf-set-selections;
@@ -75,29 +67,25 @@ if [ -z $1 ]
 #   apt install -y -qq samba smbclient;
 
 # -----------------------------------------------------------------------------------------------
- elif [ $1 = "KERNEL" ]
-  then
+ elif [ $1 = "KERNEL" ]; then
    clear;
    apt install -qq -y $RELEASE-backports;
    apt install -qq -y linux-image-amd64;
    apt install -qq -y firmware-linux;
 
 # -----------------------------------------------------------------------------------------------
- elif [ $1 = "SERVICE_OFF" ]
-  then
+ elif [ $1 = "SERVICE_OFF" ]; then
    clear;
    systemctl disable --now iscsid.service;
    systemctl disable --now open-iscsi.service;
    #systemctl disable --now smbd;
 
 # -----------------------------------------------------------------------------------------------
- elif [ $1 = "SERVICE_ON" ]
-  then
+ elif [ $1 = "SERVICE_ON" ]; then
    clear;
    systemctl enable --now iscsid.service;
    systemctl enable --now open-iscsi.service;
    #systemctl enable --now smbd;
-
 # -----------------------------------------------------------------------------------------------
  else
   echo "Script KO"
