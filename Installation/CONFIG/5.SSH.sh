b060ab39fd48f45063c6f6a03c659806868cb816
Marc <teste74@hotmail.fr>
2022-04-30 01:51:30 +0200

    Update 5.SSH.sh

--- Installation/CONFIG/5.SSH.sh
+++ Installation/CONFIG/5.SSH.sh
@@ -2,10 +2,8 @@
 # Script pour la configuration de la connexion SSH #
 ####################################################
 
-
-if [ -z $1 ]
-# ----------------------------------------------------------
- then
+# ----------------------------------------------------------------------------------------- #
+if [ -z $1 ]; then
   clear;
   echo "Argument de commande manquant.
   - 5.SSH.sh STEP_1: Création des dossiers SSH
@@ -16,25 +14,23 @@ if [ -z $1 ]
   - 5.SSH.sh STEP_6: Backup
   - 5.SSH.sh STEP_7: Recovery
   "
-# -------------------------------------------------------------------------------------------
+  
+# ----------------------------------------------------------------------------------------- #
 # Création des dossiers SSH
- elif [ $1 = "STEP_1" ]
-  then
+ elif [ $1 = "STEP_1" ]; then
    clear;
    /sbin/runuser -l root  -c 'mkdir -p /root/.ssh'
    /sbin/runuser -l $USER -c 'mkdir /home/$USER/.ssh' ;
-# -------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------- #
 # Insertion de la Clé SSH sur le serveur
- elif [ $1 = "STEP_2" ] 
-  then
+ elif [ $1 = "STEP_2" ]; then
    clear;
    /sbin/runuser -l root  -c "echo '$SSH_KEY' > /root/.ssh/authorized_keys" ;
    /sbin/runuser -l $USER -c "echo '$SSH_KEY' > /home/$USER/.ssh/authorized_keys" ;
 
-# -------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------- #
 # Modification des permissions
- elif [ $1 = "STEP_3" ]
-  then
+ elif [ $1 = "STEP_3" ]; then
    clear;
    #Root:
    chmod 700 /root/.ssh ;
@@ -43,10 +39,9 @@ if [ -z $1 ]
    chmod 700 /home/$USER/.ssh ;
    chmod 600 /home/$USER/.ssh/authorized_keys ;
 
-# -------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------- #
 # Sécurisation du SSH
- elif [ $1 = "STEP_4" ]
-  then
+ elif [ $1 = "STEP_4" ]; then
    clear;
    # Limiter l'accès SSH au Groupe / USER suivant:
    echo "AllowUsers $USER root" >>                                     /etc/ssh/sshd_config;
@@ -89,27 +84,24 @@ if [ -z $1 ]
    sed -i 's/#StrictModes yes/StrictModes yes/g'                       /etc/ssh/sshd_config;
 
    echo "Sécurisation Terminé";
-# -------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------- #
 # Relance du service
- elif [ $1 = "STEP_5" ]
-  then
+ elif [ $1 = "STEP_5" ]; then
    clear;
    systemctl restart ssh;
-# -------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------- #
 # Backup
- elif [ $1 = "STEP_6" ]
-  then
+ elif [ $1 = "STEP_6" ]; then
    clear;
    cp /etc/ssh/sshd_config /etc/ssh/sshd_config.old;
 
-# -------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------- #
 # Recovery
- elif [ $1 = "STEP_7" ]
-  then
+ elif [ $1 = "STEP_7" ]; then
    clear;
    cp /etc/ssh/sshd_config.old /etc/ssh/sshd_config;
 
-# -------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------- #
  else
   echo "Script KO"
 fi
